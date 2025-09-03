5cf2797f24c5a12d19b626aa7298d0b96ac2d44e
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-03 17:25:54 +0200

    completamento secondo esercizio ed inizio prove del terzo

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,17 +2,46 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-function ArrayFirstCapital(array) {
-    let newArray = []
-     
+let nuovoArray = []
+nuovoArray.push(arrayFirstLetter(names))
+console.log(nuovoArray)
+
+
+// Invoca la funzione qui e stampa il risultato in console
+//creo una funzione che permetta di aggiungere un array
+function arrayFirstLetter (array) {
+    let firstLetter = [];  //creo una variabile contenente un array vuoto
+
+    for (let i = 0; i < array.length; i++) {   //creo un ciclo che percorre ogni elemento dell'array
+        
+         firstLetter.push(array[i].charAt(0)) /*push all'interno dell'array vuoto della prima lettera di ogni elemnto dell'array selezionato*/
+         
+//          console.log(firstLetter)
+    }
+    return firstLetter /* faccio ritornare come risultato della funzione la vr. array vuota creata all'inizio */
+
 
 }
 
 
-// Invoca la funzione qui e stampa il risultato in console
-
-
-
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+
+
+
+
+
+
+/* SOLUZIONE :MAP */
+/* // Usiamo il metodo map per creare un nuovo array con le stringhe modificate
+let nuovoArray = names.map(function(elemento) {
+  // Verifica se l'elemento è una stringa e se è vuoto
+  if (typeof elemento === 'string' && elemento.length > 0) {
+    // Rimuove tutte le lettere tranne la prima
+    return elemento.charAt(0);
+  }
+});
+console.log(nuovoArray); */
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,33 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+let userLetter = prompt("Inserisci una lettera per cercare i nomi presenti nella lista").toUpperCase();
+console.log(`Lettera scelta dall'utente ${userLetter}`)
 
 
 // Dichiara la funzione qui.
+//creo una funzione che permetta di aggiungere un array
+function arrayForLetter (array) {
+let newArray = [];//creazione array vuoto
 
+for (let i = 0; i < array.length; i++) { //creazione ciclo
+
+    /* Se la prima lettera = a scelta utente allora push in newArray */
+    firstLetter = array[i].charAt(0)
+    console.log(firstLetter);
+
+    if (firstLetter === userLetter) {
+        newArray.push(array[i])
+    }
+    
+    return newArray
+}
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+arrayForLetter(names)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
